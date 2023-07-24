6efa2670ecf9a38ca4fb87e8505a87925d902863
예조 <berrry_99@naver.com>
2023-07-24 15:04:38 +0900

    temp

---
+++ front/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <RouterLink to="/">Home</RouterLink>
+  <RouterLink to="/write">글 작성</RouterLink>
+  <RouterView/>
+</template>
+
+<style>
+
+</style>
---
+++ front/src/views/WriteView.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const title = ref("")
+const content = ref("")
+
+const write = function () {
+
+}
+</script>
+
+<template>
+  <div>
+    <el-input v-model="title" type="text" placeholder="제목을 입력해주세요"/>
+  </div>
+
+  <div class="mt-2">
+    <el-input v-model="content" type="textarea" rows="15"></el-input>
+  </div>
+
+  <div class="mt-2">
+    <el-button type="primary" @click="write()">글 작성완료</el-button>
+  </div>
+</template>
+
+<style>
+
+</style>
